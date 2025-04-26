4807c9ac78734d40e3311250af5132c1d1e1896b
averyiaa <aaveryia@gmail.com>
2025-04-26 20:39:56 +0200

    dwm update

--- dwm/config.h
+++ dwm/config.h
@@ -1,9 +1,9 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappx     = 2;        /* gaps between windows */
+static const unsigned int gappx     = 4;        /* gaps between windows */
 static const int swallowfloating    = 0;	/* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -108,6 +108,7 @@ static const Key keys[] = {
 	/* { MODKEY,                       XK_minus,  setgaps,        {.i = -1 } }, */
 	/* { MODKEY,                       XK_equal,  setgaps,        {.i = +1 } }, */
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+    { MODKEY|ShiftMask,             XK_Return, setmfact,       {.f = 0.55} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
